e031dac8f830eb47f13239186d3891f72f2c3ec3
ashutosh <ashutoshmax04@gmail.com>
2025-04-29 17:13:44 +0000

    commited

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+
+// Respond with "Hello World" when a GET request is made to the root "/"
+
+app.get('/', (req, res) => {
+
+  res.send('Hello from Nodejs App after trigaring');
+
+});
+
+
+// Start server on port 3000 or custom port via environment variable
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+
+  console.log(`Server is running on http://localhost:${PORT}`);
+
+});
